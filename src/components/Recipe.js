3197378cd80334aa17fd15285a2f97a3a1e555e4
ericmbouwe <ericmbouwe@yahoo.fr>
2022-05-25 17:36:43 +0100

    Add recipe component styles

---
+++ src/components/Recipe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Recipe() {
+    return (
+        <div className='shadow-lg rounded min-h-[200px]'>
+            <img
+                className='object-cover max-w-xs'
+                scr=""
+                alt='cool fake alternate'
+            />
+            <p>Recipe name</p>
+        </div>
+    )
+}
+
+export default Recipe
